524e4bdf4d27ea9d8d41feac0a66e58d39f81c57
Mike Felida <mike@m3f5.dev>
2025-06-25 17:26:38 +0200

    oops

--- src/ft_lstdelone_bonus.c
+++ src/ft_lstdelone_bonus.c
@@ -6,18 +6,15 @@
 /*   By: mfelida <mfelida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 12:19:39 by mfelida           #+#    #+#             */
-/*   Updated: 2023/10/20 12:39:46 by mfelida          ###   ########.fr       */
+/*   Updated: 2025/06/25 17:25:49 by mifelida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-#include <stdlib.h>
-
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
 	if (!lst || !del)
 		return ;
-	del(lst->content);
-	free(lst);
+	del(lst);
 }
